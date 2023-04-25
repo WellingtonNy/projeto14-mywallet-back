132e14b8dc705891e63e47dfea068a69b721b62a
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-04-25 15:51:51 -0300

    PONTO DE CONTROLE REF BACK

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { MongoClient, ObjectId } from "mongodb"
+import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
 import dayjs from "dayjs"
