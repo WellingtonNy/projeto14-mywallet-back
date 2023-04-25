0be4841862db8c2c3ffaa5fafe3388f7698cd1c2
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-04-25 04:19:05 -0300

    add port

--- src/app.js
+++ src/app.js
@@ -180,6 +180,6 @@ app.get('/transacao' ,async (req,res)=>{
 })
 
 
+const PORT = process.env.PORT || 5000
 
-const PORT = 5000
-app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
